6d8d26cab5b4cabe25cff40f3732ff9d5adafe14
fardeen15 <fardeenmemon17@gmail.com>
2019-06-21 07:47:49 -0700

    change on firebase

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-// var metalObj = [
-// {
-//     name: "Silver",
-//     code: "SI"
-// },
-// {
-//     name: "Gold",
-//     code: "G"
-// },
-// {
-//     name: "Palladium",
-//     code: "P"
-// }
-// ]
-// var obj = [
-// {
-//     name: "ladies ring",
-//     code: "lR"
-// },
-// {
-//     name: "Gents ring",
-//     code: "GR"
-// },
-// {
-//     name: "Name Locket",
-//     code: "NL"
-// }
-// ]
-function metalConstructor(name, code) {
-    this.name = name,
-        this.code = code
-}
+var firebaseConfig = {
+    apiKey: "AIzaSyDZJ-51vMxB8PTlGZhTgkS2ftxJ3N_khkw",
+    authDomain: "new-work-ccbe7.firebaseapp.com",
+    databaseURL: "https://new-work-ccbe7.firebaseio.com",
+    projectId: "new-work-ccbe7",
+    storageBucket: "",
+    messagingSenderId: "688164292556",
+    appId: "1:688164292556:web:3eca004aac27a9f0"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+// var metalObj = []
 function addMetal() {
     var div = document.getElementById("mainDiv3");
     if (div.innerHTML === "") {
@@ -47,18 +29,20 @@ function addMetal() {
     }
     div.style.display = "block";
 }
+function metalConstructor(name, code) {
+    this.name = name,
+        this.code = code
+}
 var inp1;
 var inp2;
 var metalCon;
 function addvalue() {
     event.preventDefault();
-    // window.stop();
     inp1 = document.getElementById("exampleInputEmail2").value;
     inp2 = document.getElementById("exampleInputEmail1").value;
-
-    metalCon = new metalConstructor(inp1, inp2);
-    // metalObj.push(metalCon);
-    // console.log(metalObj);
+    metalCon = new metalConstructor(inp1, inp2.toUpperCase());
+    // metalObj.push(metalCon)
+    database.ref().child('data').child('metal').child(inp1).set(metalCon)
 
     document.getElementById("exampleInputEmail2").value = "";
     document.getElementById("exampleInputEmail1").value = "";
@@ -68,77 +52,26 @@ function addvalue() {
     document.getElementById("mainDiv3").style.display = "none";
 
 }
-var obj1 = JSON.parse(localStorage.getItem("data"));
-
+var metalName;
 function metal() {
     var id = document.getElementById("inputGroupSelect01");
-
-    for (var i = 0; i < obj1.length; i++) {
-        id.innerHTML += `
-        <option value="${i}">${obj1[i].name}</option>
+    database.ref().child('data').child('metal').on('value', (snap) => {
+        var data = Object.values(snap.val())
+        for (var i = 0; i < data.length; i++) {
+            metalName = data[i].name;
+            id.innerHTML += `
+        <option value="${data[i].name}">${data[i].name}</option>
         `
-    }
-    var obj = JSON.parse(localStorage.getItem("data"))
-    if (obj === null) {
-        obj = []
-    }
-    obj.push(metalCon);
-    localStorage.setItem("data", JSON.stringify(obj));
-}
+        }
 
-function select2() {
-    var id = document.getElementById("inputGroupSelect01");
-    if (id.value === null) {
-        metal();
-    }
+    })
 
 }
-select2()
-
-
-
-
-
-
-
-
-
-function itemCode() {
-    var index = document.getElementById("inputGroupSelect01").value;
-    var code = obj1[index].code
-
-    var index1 = document.getElementById("inputGroupSelect02").value;
-    var code1 = obj2[index1].code
-
-    var date = new Date().getFullYear();
-    var covert = date.toString().slice(2);
-
-    var input = document.getElementById("inlineFormInputGroup");
-    input.value = code + code1 + covert + "001";
-
-}
-
-
-function itemConstructor(name, code) {
-    this.name = name,
-        this.code = code
-}
-
-
+metal()
 function select1() {
     document.getElementById("inputGroupSelect02").removeAttribute("disabled");
-    var id = document.getElementById("inputGroupSelect02");
-    if (id.value === null) {
-        item();
-    }
+    item()
 }
-
-
-
-
-
-
-
 
 
 function addItem() {
@@ -158,38 +91,68 @@ function addItem() {
     }
     div.style.display = "block";
 }
-var itemCon;
+function itemConstructor(name, code) {
+    this.name = name,
+        this.code = code
+}
+
 function objAdd() {
     event.preventDefault();
-    
+
     var value = document.getElementById("exampleInputEmail3").value;
     var value2 = document.getElementById("exampleInputEmail4").value;
-    
-    
-    itemCon = new itemConstructor(value, value2);
-    // obj.push(itemCon);
-    
+
+
+    var itemCon = new itemConstructor(value, value2.toUpperCase());
+    database.ref().child('data').child('item').child(value).set(itemCon)
+
+
     document.getElementById("exampleInputEmail3").value = "";
     document.getElementById("exampleInputEmail4").value = "";
-    
+
     document.getElementById("inputGroupSelect02").innerHTML = "";
     document.getElementById("mainDiv2").style.display = "none";
     item();
 }
-var obj2 = JSON.parse(localStorage.getItem("data1"));
+var itemName;
 function item() {
     var id = document.getElementById("inputGroupSelect02");
+    id.innerHTML = ""
+    database.ref().child('data').child('item').on('value', (snap) => {
+        var data = Object.values(snap.val())
+        for (var i = 0; i < data.length; i++) {
+            itemName = data[i].name;
+            id.innerHTML += `
+            <option value="${data[i].name}">${data[i].name}</option>
+            `
+        }
+    })
 
-    var obj = JSON.parse(localStorage.getItem("data1"))
-    if (obj === null) {
-        obj = []
-    }
-    obj.push(itemCon);
-    localStorage.setItem("data1", JSON.stringify(obj));
-    for (var i = 0; i < obj2.length; i++) {
-        id.innerHTML += `
-              <option value="${i}">${obj2[i].name}</option>
-                            `
-    }
+}
+function itemCode(){
+var index = document.getElementById("inputGroupSelect01").value;
+var index1 = document.getElementById("inputGroupSelect02").value;
+// console.log(index1)
+let code1;
+let code2;
+database.ref().child('data').child('metal').child(index).on('value', (snap) => {
+    var data = Object.values(snap.val())
+    code1 = data[0];
+    // for (var i = 0; i < data.length; i++) {
+    //     console.log(data[i].code)
+    // }
+})
+database.ref().child('data').child('item').child(index1).on('value', (snap) => {
+    var data = Object.values(snap.val())
+    code2 = data[0];
+    // for (var i = 0; i < data.length; i++) {
+    //     console.log(data[i])
+    // }
+})
+    var date = new Date().getFullYear();
+    var covert = date.toString().slice(2);
+
+    var input = document.getElementById("inlineFormInputGroup");
+    input.value = code1 + code2 + covert + "001";
 
 }
